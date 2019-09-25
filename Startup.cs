f2fb450d7e0d7b5416e7a3bce0d6aa3b427fb251
Kirk Larkin <serpent5@pm.me>
2019-09-25 22:08:19 +0100

    Removed redundant Google+ workaround.

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,14 +22,6 @@ namespace SO53654020
                 {
                     o.ClientId = "...";
                     o.ClientSecret = "...";
-                    o.UserInformationEndpoint = "https://www.googleapis.com/oauth2/v2/userinfo";
-                    o.ClaimActions.Clear();
-                    o.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-                    o.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-                    o.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "given_name");
-                    o.ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_name");
-                    o.ClaimActions.MapJsonKey("urn:google:profile", "link");
-                    o.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
                 });
 
             serviceCollection.AddMvc()
@@ -43,6 +32,7 @@ namespace SO53654020
         public void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment)
         {
             applicationBuilder.UseDeveloperExceptionPage();
+            applicationBuilder.UseHttpsRedirection();
             applicationBuilder.UseAuthentication();
             applicationBuilder.UseMvcWithDefaultRoute();
         }
